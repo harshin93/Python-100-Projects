73e38fc1ca008a05bd56a6a12e6bfd7f69cc685f
Harshin Mehta <harshinmehta@gmail.com>
2020-06-23 20:08:48 -0700

    pylint

--- alarm_clock.py
+++ alarm_clock.py
@@ -9,7 +9,7 @@ current_sec  = int(strftime("%S",localtime()))
 current_min  = int(strftime("%M",localtime()))
 print(current_min, current_sec)
 
-def myalarm(minute_alarm, second_alarm): 
+def myalarm(minute_alarm, second_alarm):
      additional_time = int(minute_alarm * 60 + second_alarm) #merge minute_alarm and second_alarm into 00:00 and add it to current_time
      print("Sleeping for seconds {}".format(additional_time))
      sleep(additional_time)
@@ -19,18 +19,3 @@ def myalarm(minute_alarm, second_alarm):
 
 myalarm(0,5)
 
-
-    
-'''
-if len(mytime)==3:
-        sounds= mytime[0]*60*60+mytime[1]*60+mytime[2]
-        time.sleep(sounds)
-        frequency = 2500
-        duration = 1000
-        winsound.Beep(frequency,duration)
-    else:
-        print("Write the time in correct format")
-        
-    #except Exception as e:
-        #print('this is the exception',e,'please enter correct details')
-'''
